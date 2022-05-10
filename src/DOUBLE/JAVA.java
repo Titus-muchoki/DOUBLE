11b40735316c51991c5014a2b17c3498f8a842a4
kajela <titoyut56@gmail.com>
2022-05-10 06:42:00 -0400

    Initial commit

---
+++ src/DOUBLE/JAVA.java
@@ -0,0 +1,10 @@
+package DOUBLE;
+
+public class JAVA {
+    public void myHouse() {
+        System.out.println("i just bought a house in kilimani");
+    }
+    public void costPrice(int cost) {
+        System.out.println("it costed me: "+cost);
+    }
+}
---
+++ src/DOUBLE/second.java
@@ -0,0 +1,10 @@
+package DOUBLE;
+
+ class second {
+        public static void main(String[] args) {
+         JAVA myObj = new JAVA();
+            myObj.myHouse();
+            myObj.costPrice(40000);
+        }
+    }
+
